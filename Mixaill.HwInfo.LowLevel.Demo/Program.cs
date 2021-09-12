ba81bfd683265578b15624b9f4b7f1f363eb9043
Mikhail Paulyshka <me@mixaill.tk>
2021-09-12 12:58:21 +0300

    LowLevel.Demo: ad RDTSCP demo

--- Mixaill.HwInfo.LowLevel.Demo/Program.cs
+++ Mixaill.HwInfo.LowLevel.Demo/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Mixaill.HwInfo.LowLevel.PCI;
 using System;
+using System.Diagnostics;
 
 namespace Mixaill.HwInfo.LowLevel.Demo
 {
@@ -7,10 +8,61 @@ namespace Mixaill.HwInfo.LowLevel.Demo
     {
         static void Main(string[] args)
         {
-            var pc = new Pci();
-            pc.Print();
+            ulong f = Native.Rdtsc.Frequency();
+            Console.WriteLine($"RDTSC frequency: {Math.Round(f/1e6,3)} MHz");
+            Console.WriteLine($"RDTSC tick     : {Math.Round(1e9/f,3)} ns");
 
-            Console.ReadKey();
+            Console.WriteLine("");
+
+            Bench_RDTSCP();
+            Bench_StopWatch();
+            Bench_TickCount();
+
+            return;
+        }
+
+        private static void Bench_RDTSCP()
+        {
+            ulong count = Native.Rdtsc.Test();
+            Console.WriteLine($"RDTSCP: {count}, {Math.Round(1e9 / count,3)} ns");
+        }
+
+        private static void Bench_StopWatch()
+        {
+            //warmup
+            for (int i = 0; i < 100; i++)
+            {
+                Stopwatch.GetTimestamp();
+            }
+
+            //count
+            ulong count = 0;
+            long doneTick = Stopwatch.GetTimestamp() + TimeSpan.TicksPerSecond;
+            while (Stopwatch.GetTimestamp() <= doneTick)
+            {
+                count++;
+            }
+
+            Console.WriteLine($"StopWatch: {count}, {Math.Round(1e9 / count,3)} ns");
+        }
+
+        private static void Bench_TickCount()
+        {
+            //warmup
+            for (int i = 0; i < 100; i++)
+            {
+                Stopwatch.GetTimestamp();
+            }
+
+            //count
+            ulong count = 0;
+            int doneTick = Environment.TickCount + 1000;
+            while (Environment.TickCount <= doneTick)
+            {
+                count++;
+            }
+
+            Console.WriteLine($"TickCount: {count}, {Math.Round(1e9 / count,3)} ns");
         }
     }
 }
---
+++ Mixaill.HwInfo.LowLevel.Demo/d1_pci_enum.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+using Mixaill.HwInfo.LowLevel.PCI;
+
+namespace Mixaill.HwInfo.LowLevel.Demo
+{
+    class d1_pci_enum
+    {
+        public static void process()
+        {
+            var pc = new Pci();
+            pc.Print();
+        }
+    }
+}
